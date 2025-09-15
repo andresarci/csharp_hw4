e4ff1a29ec1ab44bdd8325de419dc22a84299203
Andres Arciniegas <andresarciniegas@Andress-Air.lan>
2025-09-14 20:32:45 -0400

    adding hurricane exercise

---
+++ Hurricane/Hurricane.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+namespace Hurricane;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Please insert the speed of the wind in mi/h");
+        int speed = Convert.ToInt32(ReadLine());
+        if (speed < 74)
+        {
+            WriteLine("Not a hurrcane yet");
+        }
+        else if (speed >= 74 && speed <= 95)
+        {
+            WriteLine("Category 1");
+        }
+        else if (speed >= 96 && speed <= 110)
+        {
+            WriteLine("Category 2");
+        }
+        else if (speed >= 111 && speed <= 129)
+        {
+            WriteLine("Category 3");
+        }
+        else if (speed >= 130 && speed <= 156)
+        {
+            WriteLine("Category 4");
+        }
+        else if (speed >= 157)
+        {
+            WriteLine("Category 5");
+        }
+    }
+}
