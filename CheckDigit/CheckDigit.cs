c554611244a590ad17100dadf7fb08a2dd48e17b
Andres Arciniegas <andresarciniegas@Andress-Air.lan>
2025-09-14 22:36:43 -0400

    adding exercise 6

---
+++ CheckDigit/CheckDigit.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+namespace CheckDigit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WriteLine("Please insert your 4 digits account number");
+        int AccountNumber = Convert.ToInt32(ReadLine());
+        int FirstThree = AccountNumber / 10;
+        int LastDigit = AccountNumber % 10;
+        int reminder = FirstThree % 7;
+        if (LastDigit == reminder)
+        {
+            WriteLine("Your account number is valid");
+        }
+        else
+        {
+            WriteLine("Invalid");
+        }
+    }
+}
+ 
